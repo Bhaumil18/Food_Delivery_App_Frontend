f0d429f898cb9c044d4d1f87ec2b2c49a4e29100
Bhaumil18 <bhaumil1853@gmail.com>
2024-03-30 21:05:41 +0530

    Menu Page -u

--- src/actions/Restaurant.jsx
+++ src/actions/Restaurant.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const getRestaurantData = ({ id }) => async (dispatch) => {
     try {
-        const { data } = await axios.get(`http://localhost:5000/api/restaurants/details/${id}`);
+        // const { data } = await axios.get(`http://localhost:5000/api/restaurants/details/${id}`);
+        const { data } = await axios.get(`https://food-delivery-app-backend-3oiy.onrender.com/api/restaurants/details/${id}`);
         // console.log(data.restaurant);
         dispatch(setRestData(data.restaurant));
     } catch (error) {
--- src/actions/Restaurants.jsx
+++ src/actions/Restaurants.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 const FetchAllRest = ({ city, searchBy, page, cuisines,sortOption }) => async (dispatch) => {
     try {
         dispatch(FetchResReq());
-        // const Restaurants = await axios.get(`https://food-delivery-app-backend-3oiy.onrender.com/api/restaurants/${city}`, {
-        const Restaurants = await axios.get(`http://localhost:5000/api/restaurants/${city}`, {
+        const Restaurants = await axios.get(`https://food-delivery-app-backend-3oiy.onrender.com/api/restaurants/${city}`, {
+        // const Restaurants = await axios.get(`http://localhost:5000/api/restaurants/${city}`, {
             params: {
                 searchBy: searchBy,
                 page: page,
